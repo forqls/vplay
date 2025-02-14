02b6e1034ebe777fb301ea91af612848c0f9a99e
salgu <warsfac@gmail.com>
2025-02-14 15:20:05 +0900

    250214 스프링부트 서버 구축 완료

---
+++ src/main/java/pocopoco_vplay/VplayApplication.java
@@ -0,0 +1,13 @@
+package pocopoco_vplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pocopoco_vplay/VplayApplicationTests.java
@@ -0,0 +1,13 @@
+package pocopoco_vplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
