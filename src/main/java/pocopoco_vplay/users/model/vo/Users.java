d7a63f87c934817cf5fa11fbd5ffe3895a651d46
효바니 <dksggyqls@gmail.com>
2025-02-17 13:11:35 +0900

    유저 vo 생성

--- src/main/java/pocopoco_vplay/users/model/vo/Users.java
+++ src/main/java/pocopoco_vplay/users/model/vo/Users.java
@@ -1,5 +1,29 @@
 package pocopoco_vplay.users.model.vo;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Users {
-	
+	private int userNo;
+    private String userId;
+    private String userPw;
+    private String userNickname;
+    private String userName;
+    private String userPhone;
+    private String userEmail;
+    private Date userBrith;
+    private String userGender;
+    private String status;
+    private String isAdmin;
+    private Date joinDate;
+    private String userComment;
+    private String userCreatorRank;
+    private String userPlan;
+    private String userProfile;
 }
