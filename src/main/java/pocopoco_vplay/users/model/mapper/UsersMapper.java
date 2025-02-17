a40867b6aeb7a6b6f811e65c49b0460088551e4e
Sondong22 <vlwhsvk159@naver.com>
2025-02-17 19:29:56 +0900

    회원가입

--- src/main/java/pocopoco_vplay/users/model/mapper/UsersMapper.java
+++ src/main/java/pocopoco_vplay/users/model/mapper/UsersMapper.java
@@ -2,9 +2,13 @@ package pocopoco_vplay.users.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import pocopoco_vplay.users.model.vo.Users;
+
 @Mapper
 public interface UsersMapper {
 
 	int checkId(String id);
+
+	int insertUser(Users user);
 
 }
--- src/main/java/pocopoco_vplay/users/model/service/UsersService.java
+++ src/main/java/pocopoco_vplay/users/model/service/UsersService.java
@@ -21,4 +21,8 @@ public class UsersService {
 		return null;
 	}
 
+	public int insertUser(Users user) {
+		return mapper.insertUser(user);
+	}
+
 }
