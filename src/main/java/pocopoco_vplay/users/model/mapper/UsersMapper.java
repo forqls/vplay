c217be7fcf9107df798233b2745354a9fb0a8b88
효바니 <dksggyqls@gmail.com>
2025-02-17 19:57:54 +0900

    로그아웃 완료

--- src/main/java/pocopoco_vplay/users/model/mapper/UsersMapper.java
+++ src/main/java/pocopoco_vplay/users/model/mapper/UsersMapper.java
@@ -3,14 +3,10 @@ package pocopoco_vplay.users.model.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import pocopoco_vplay.users.model.vo.Users;
 
-import pocopoco_vplay.users.model.vo.Users;
-
 @Mapper
 public interface UsersMapper {
 
 	int checkId(String id);
 
-  Users signIn(Users user);
-
-	int insertUser(Users user);
-
+    Users signIn(Users user);
+}
