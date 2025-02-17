68f32167ec5e96cbefc241e188045640b7b1ab20
효바니 <dksggyqls@gmail.com>
2025-02-17 15:50:08 +0900

    SecurityConfig 생성 및 로그인 구현중

--- src/main/java/pocopoco_vplay/users/controller/UsersController.java
+++ src/main/java/pocopoco_vplay/users/controller/UsersController.java
@@ -1,20 +1,26 @@
 package pocopoco_vplay.users.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import pocopoco_vplay.users.exception.UsersException;
 import pocopoco_vplay.users.model.service.UsersService;
+import pocopoco_vplay.users.model.vo.Users;
 
 @Controller
 @RequestMapping("/users/")
 @RequiredArgsConstructor
+@SessionAttributes("loginUser")
 public class UsersController {
 	private final UsersService uService;
-	
-	
+
+    private final BCryptPasswordEncoder bcrypt;
 	
 	
 	
@@ -34,18 +40,80 @@ public class UsersController {
 	public int checkId(@RequestParam("id") String id) {
 		return uService.checkId(id);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @PostMapping("/users/signIn")
+    public String login(Users user, Model model, @RequestParam("beforeURL") String beforeURL){
+        Users loginUser = usersService.login(user);
+        if(loginUser != null && bcrypt.matches(user.getUserPw(), loginUser.getUserPw())){
+            model.addAttribute("loginUser", loginUser);
+
+            if(loginUser.getIsAdmin().equals("Y")){
+                return "redirect:/admin/index";
+            }else{
+                return "redirect:" + beforeURL;
+            }
+        } else{
+            throw new UsersException("로그인을 실패하였습니다.");
+        }
+    }
+
+
 }
