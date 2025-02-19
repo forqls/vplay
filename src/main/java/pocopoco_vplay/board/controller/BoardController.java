0ff1fb8178d37d490dda7575f02eabc8299e21c3
salgu <warsfac@gmail.com>
2025-02-19 18:33:22 +0900

    250219 비디오 템플릿으로 이동

--- src/main/java/pocopoco_vplay/board/controller/BoardController.java
+++ src/main/java/pocopoco_vplay/board/controller/BoardController.java
@@ -1,5 +1,21 @@
 package pocopoco_vplay.board.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/board/")
+@RequiredArgsConstructor
+@SessionAttributes("loginUser")
+public class BoardController {
+	
+	
+	@GetMapping("video-templates-list")
+	public String joinVideoTemplatesList() {
+		return "videoTemplates_list";
+	}
 }
