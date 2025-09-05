52a5e34b015d5c8740c3f76ee0f45abfbb6797f2
Hyobin An <dksggyqls@gmail.com>
2025-09-06 04:08:36 +0900

    환경변수 설정 및 구글 비번 인증방식 변경

--- src/main/java/pocopoco_vplay/commom/config/Mailconfig.java
+++ src/main/java/pocopoco_vplay/commom/config/Mailconfig.java
@@ -1,11 +1,11 @@
 package pocopoco_vplay.commom.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 public class Mailconfig {
@@ -15,16 +15,19 @@ public class Mailconfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com"); // 메일 호스트
         mailSender.setPort(587); // 메일 포트번호
-        mailSender.setUsername("poco.vplay@gmail.com"); // 웹사이트 메일
-        mailSender.setPassword("nhcp lskv drhd jhtz"); // 앱 비밀번호
+
+        // application.properties에서 직접 읽어오므로 여기서는 설정 x.
+        // mailSender.setUsername("your-email@gmail.com");
+        // mailSender.setPassword("your-password");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
+        // OAuth 2.0을 사용하도록 설정
+        props.put("mail.smtp.auth.mechanisms", "XOAUTH2");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "true"); // 디버그 필요시 true로 설정
 
         return mailSender;
     }
-
 }
