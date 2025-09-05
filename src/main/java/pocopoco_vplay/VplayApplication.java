be4fce96effc4673a9018e97f1a974e63614521d
Hyobin An <dksggyqls@gmail.com>
2025-09-06 06:13:42 +0900

    Spring Boot Security 자동 설정이 비활성화 끔

--- src/main/java/pocopoco_vplay/VplayApplication.java
+++ src/main/java/pocopoco_vplay/VplayApplication.java
@@ -6,9 +6,8 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication()
 public class VplayApplication {
 
 	public static void main(String[] args) {
