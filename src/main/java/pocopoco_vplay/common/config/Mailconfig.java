9a130be9f4e030d90f77d8345b98343529fc0408
Hyobin An <dksggyqls@gmail.com>
2025-09-12 19:44:19 +0900

    지메일 설정 변경

--- src/main/java/pocopoco_vplay/common/config/Mailconfig.java
+++ src/main/java/pocopoco_vplay/common/config/Mailconfig.java
@@ -13,21 +13,8 @@ public class Mailconfig {
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com"); // 메일 호스트
-        mailSender.setPort(587); // 메일 포트번호
-
-        // application.properties에서 직접 읽어오므로 여기서는 설정 x.
-        // mailSender.setUsername("your-email@gmail.com");
-        // mailSender.setPassword("your-password");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        // OAuth 2.0을 사용하도록 설정
-        props.put("mail.smtp.auth.mechanisms", "XOAUTH2");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true"); // 디버그 필요시 true로 설정
-
+        // 호스트와 포트 설정 등은 application.properties에서 자동으로 읽어옴
+        // 별도 설정 없이 반환만
         return mailSender;
     }
 }
